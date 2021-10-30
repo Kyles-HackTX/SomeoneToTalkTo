e7c43b0eda1f377ef7214fe38864c52d924c7344
tm1287 <tejas.maraliga@gmail.com>
2021-10-30 17:40:03 -0500

    Print JSON data from client POST

--- test-client/index.js
+++ test-client/index.js
@@ -1,14 +1,19 @@
-const { default: axios } = require("axios");
+let axios = require("axios");
 let fs = require("fs");
+var FormData = require('form-data');
 let wav = require("node-wav");
 
 let buffer = fs.readFileSync("test.wav");
-let result = wav.decode(buffer);
 
-console.log(result.sampleRate);
-console.log(result.channelData);
+console.log(buffer);
 
-axios.post("http://127.0.0.1:5000/upload", {
+let formData = new FormData();
+formData.append('wavfile', buffer);
+
+axios.post("http://127.0.0.1:5000/upload", 
+    { 
+        data: formData,
+        timestamp: Date.now(),
     }).then((resp) => {
         console.log(resp.data);
     }).catch((err) => {
