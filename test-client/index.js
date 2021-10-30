e56ee8d9a38b0e2dfbd84eb73e7738cf88eab190
tm1287 <tejas.maraliga@gmail.com>
2021-10-30 15:57:32 -0500

    Add axios post to test client

--- test-client/index.js
+++ test-client/index.js
@@ -1,8 +1,17 @@
+const { default: axios } = require("axios");
 let fs = require("fs");
 let wav = require("node-wav");
 
 let buffer = fs.readFileSync("test.wav");
 let result = wav.decode(buffer);
 
-console.log(result.sampleData);
+console.log(result.sampleRate);
 console.log(result.channelData);
+
+axios.post("http://127.0.0.1:5000/upload", {
+    }).then((resp) => {
+        console.log(resp.data);
+    }).catch((err) => {
+        console.log(err);
+    }
+);
