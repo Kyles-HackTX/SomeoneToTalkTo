11afcdbe73ede5cb955ffcbd86d5b371797c76f8
tm1287 <tejas.maraliga@gmail.com>
2021-10-30 21:34:40 -0500

    Get json from test client

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request
+from flask_restful import reqparse, abort, Api, Resource
+import json
 
 app = Flask(__name__)
 api = Api(app)
+
 
 class AudioUploadResource(Resource):
     def get(self):
         return {'hello': 'GET'}
     def post(self):
+        data = request.get_json()
+        return {'hello': 'POST'}
         # Validate the request
         # Capture the audio data from the request
+
+
         # invoke the necessary functions on the model
 
         # Return response and status code
-        return {'hello': 'POST'}
 api.add_resource(AudioUploadResource, '/upload')
 
 if __name__ == '__main__':
