b762122d02a6641270cbb458ce306743518e0a78
tm1287 <tejas.maraliga@gmail.com>
2021-10-31 11:43:17 -0500

    Add ffmpeg %d

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ api = Api(app)
 predict = Predict()
 
 def convert_and_split(filename):
-    command = ['ffmpeg', '-i', filename, '-f', 'segment', '-segment_time', '15', 'output.wav']
+    command = ['ffmpeg', '-i', filename, '-f', 'segment', '-segment_time', '15', 'out%09d.wav']
     subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
 
 class AudioUploadResource(Resource):
