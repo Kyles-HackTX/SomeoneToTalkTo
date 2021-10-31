0da665e4f3533daa684f63b16f9c0a05a817aa68
tm1287 <tejas.maraliga@gmail.com>
2021-10-31 09:33:01 -0500

    Try saving uploaded file to machine again

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,7 +13,7 @@ class AudioUploadResource(Resource):
         data = request.get_json()
         print(data)
         print(request.files["audio_data"])
-        request.files["audio_data"].save(request.files["audio_data"].filename)
+        request.files["audio_data"].save("/tmp/audio.wav")
         return {'hello': 'POST'}
         # Validate the request
         # Capture the audio data from the request
