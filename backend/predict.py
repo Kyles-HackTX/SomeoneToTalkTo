3b23230590b57879f34126f60c5c51f132e4c538
suraj_jena <suraj_jena@persistent.com>
2021-10-31 11:06:53 -0500

    Inference callable to load serialized model + return predicted label

---
+++ backend/predict.py
@@ -0,0 +1,37 @@
+import torch
+import librosa
+import librosa.display
+from librosa.feature.spectral import mfcc
+
+
+class Predict():
+    def __init__(self):
+        self.model = torch.load("./mvp.zip")
+        self.model.eval()
+
+    def __call__(self, wav_file):
+        X, sample_rate = librosa.load(wav_file,
+                                      res_type='kaiser_fast',
+                                      duration=2.5,
+                                      sr=44100,
+                                      offset=0.5
+        )
+        sample_rate = np.array(sample_rate)
+
+        mfccs = np.mean(
+            librosa.feature.mfcc(y=X,
+                                 sr=sample_rate,
+                                 n_mfcc=13),
+            axis=0
+        )
+
+        example = torch.from_numpy(mfccs)
+        example = torch.unsqueeze(torch.unsqueeze(example, dim=0), dim=0)
+        print(example.shape, example)
+        outputs = self.model(example.float())
+        print(outputs.data)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        # print(le_name_mapping[max(predicted)])
+
+        return name_le_mapping[predicted.item()]
